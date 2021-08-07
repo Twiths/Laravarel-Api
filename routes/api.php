14ed656823d7165a3351d348dbbbca6605197328
Kally <kallyasmedia@gmail.com>
2021-08-07 16:15:22 +0000

    Add controllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Article;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', [ArticleController::class,'index']);
+Route::get('articles/{article}', [ArticleController::class, 'show']);
+Route::post('articles', [ArticleController::class, 'store']);
+Route::put('articles/{article}', [ArticleController::class, 'update']);
+Route::delete('articles/{article}', [ArticleController::class, 'delete']);
